1cff0201f250247b00ee4f310146135fb0c08bea
ray-lei <r.lei@sap.com>
2024-01-15 15:45:56 -0800

    add gate

--- level_1.gd
+++ level_1.gd
@@ -9,7 +9,6 @@ func newGame():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	newGame()
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
 	pass
